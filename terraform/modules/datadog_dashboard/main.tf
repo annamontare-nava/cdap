73647063c00db4b165717c669f677681a0430397
Anna Montare <267455234+annamontare-nava@users.noreply.github.com>
2026-07-29 06:14:47 -0600

    replace more occurrences of $env

--- terraform/modules/datadog_dashboard/main.tf
+++ terraform/modules/datadog_dashboard/main.tf
@@ -515,13 +515,13 @@ resource "datadog_dashboard" "application_metrics_dashboard" {
               query {
                 metric_query {
                   name  = "query1"
-                  query = "sum:trace.${var.apm_primary_operation}.errors{application:${var.app}, $env} by {service}.as_count()"
+                  query = "sum:trace.${var.apm_primary_operation}.errors{application:${var.app}, $environment} by {service}.as_count()"
                 }
               }
               query {
                 metric_query {
                   name  = "query2"
-                  query = "sum:trace.${var.apm_primary_operation}.hits{application:${var.app}, $env}.as_count()"
+                  query = "sum:trace.${var.apm_primary_operation}.hits{application:${var.app}, $environment}.as_count()"
                 }
               }
               formula {
